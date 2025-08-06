a2d762b5bb0fd2eaed67273ae6121d7e2e8f2f59
Yogesh21v <yogeshvenkat21@gmail.com>
2025-08-06 21:42:59 +0530

    fix: clean final cloudapp with single app.run

--- cloudapp.py
+++ cloudapp.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import os
 
-
 app = Flask(__name__)
 
 # Store data in memory
@@ -20,5 +19,6 @@ def get_params():
     return jsonify(stored_data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
